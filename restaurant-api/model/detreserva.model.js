a9426cab1be158911ba9e2d7ea660001730e5f82
bsramosl <steeven10f@gmail.com>
2023-12-16 22:18:18 -0500

    det cart

--- restaurant-api/model/detreserva.model.js
+++ restaurant-api/model/detreserva.model.js
@@ -10,14 +10,42 @@ const DetReserva = {
   getList: (callback) => {
     connection.query('SELECT * FROM detalle_reserva', callback);
   },
-  
+
   get: (id, callback) => {
     connection.query('SELECT * FROM detalle_reserva dp JOIN menu m ON dp.id_menu = m.id_menu WHERE dp.id_detalle_reserva = ?', [id], callback);
-  }, 
-
-  create: (newPedido, callback) => {
-    connection.query('INSERT INTO detalle_reserva SET ?', newPedido, callback);
   },
+
+  create: (newPedido, callback) => { 
+    let idReserva;
+    const reservaValues = [
+        [newPedido.id_usuario, newPedido.fecha_reserva, newPedido.estado, newPedido.codigo_estado]
+    ];
+
+    connection.query('INSERT INTO reserva (id_usuario, fecha_reserva, estado, codigo_estado) VALUES ?', [reservaValues], (error, result) => {
+       if (error) {
+         callback(error, null);
+         return;
+       } else {
+         idReserva = result.insertId;  
+         const detalles = newPedido.detalles.map(detalle => [
+           idReserva,
+           detalle.id_menu,
+           detalle.cantidad,
+           detalle.precio_unitario,
+           detalle.subtotal,
+         ]);  
+         connection.query('INSERT INTO detalle_reserva (id_reserva, id_menu, cantidad, precio_unitario, subtotal) VALUES ?', [detalles], (error, result) => {
+           if (error) {
+             callback(error, null);
+           } else {
+            console.log(result)
+             callback(null, result);
+           }
+         });
+       }
+    });
+  },
+
   update: (id, update, callback) => {
     connection.query('UPDATE detalle_reserva SET ? WHERE id_detalle_reserva = ?', [update, id], callback);
   },
