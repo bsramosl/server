94389e0129095423cce03a0cc889514585cdadf7
Bryan Steeven Ramos López <bryan.ramos@fbim-consultor.com>
2023-12-29 11:09:54 -0500

    camibos

--- restaurant-api/model/bar.model.js
+++ restaurant-api/model/bar.model.js
@@ -7,11 +7,11 @@ connection.connect();
 
 const Bar = {
   getList: (callback) => {
-    connection.query('SELECT * FROM bar ba JOIN ubicacion ub ON ba.id_ubicacion = ub.id_ubicacion', callback);
+    connection.query('SELECT * FROM bar ba  INNER JOIN ubicacion ub ON ba.id_ubicacion = ub.id_ubicacion', callback);
   },
 
   get: (id, callback) => {
-    connection.query('SELECT * FROM bar WHERE id_bar = ?', [id], callback);
+    connection.query('SELECT * FROM bar ba INNER JOIN ubicacion ub ON ba.id_ubicacion = ub.id_ubicacion WHERE id_bar = ?', [id], callback);
   },
   create: (newBar, callback) => {
     let idUbicacion;
@@ -43,8 +43,33 @@ const Bar = {
    }); 
   },
 
-  update: (id, updatedBar, callback) => {
-    connection.query('UPDATE bar SET ? WHERE id_bar = ?', [updatedBar, id], callback);
+  update: (id, updatedBar, callback) => {  
+    const ubicacion = [
+        [updatedBar.nombre_bar,updatedBar.latitud,updatedBar.longitud]
+    ];
+    console.log(ubicacion)
+    connection.query('UPDATE ubicacion SET nombre_ubicacion = ?, latitud = ?, longitud = ? WHERE id_ubicacion = ?', [updatedBar.nombre_bar, updatedBar.latitud, updatedBar.longitud, updatedBar.id_ubicacion], (error, result) => {
+      if (error) {
+        callback(error, null);
+        return;
+      } else {
+        const bar =[[          
+          updatedBar.nombre_bar,
+          updatedBar.id_ubicacion,
+          updatedBar.desayuno_horario,
+          updatedBar.almuerzo_horario,
+          updatedBar.merienda_horario,
+        ]
+        ];  
+        connection.query('UPDATE ubicacion SET nombre_bar = ?, id_ubicacion = ?, desayuno_horario = ?, almuerzo_horario = ?, merienda_horario = ? WHERE id_bar = ?', [updatedBar.nombre_bar,updatedBar.id_ubicacion, updatedBar.desayuno_horario,updatedBar.almuerzo_horario,updatedBar.merienda_horario,id], (error, result) => {
+          if (error) {
+            callback(error, null);
+          } else {
+            callback(null, result);
+          }
+        });
+      }
+    });
   },
 
   delete: (id, callback) => {
