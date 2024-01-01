5c969e50013704043e156dd5b543e330a945a5db
bsramosl <steeven10f@gmail.com>
2024-01-01 15:22:35 -0500

    bar

--- restaurant-api/model/bar.model.js
+++ restaurant-api/model/bar.model.js
@@ -11,8 +11,9 @@ const Bar = {
   },
 
   get: (id, callback) => {
-    connection.query('SELECT * FROM bar WHERE id_bar = ?', [id], callback);
+    connection.query('SELECT * FROM bar ba INNER JOIN ubicacion ub ON ba.id_ubicacion = ub.id_ubicacion WHERE id_bar = ?', [id], callback);
   },
+
   create: (newBar, callback) => {
     let idUbicacion;
     const ubicacion = [
@@ -43,8 +44,32 @@ const Bar = {
    }); 
   },
 
-  update: (id, updatedBar, callback) => {
-    connection.query('UPDATE bar SET ? WHERE id_bar = ?', [updatedBar, id], callback);
+  update: (id, updatedBar, callback) => {  
+    const ubicacion = [
+        [updatedBar.nombre_bar,updatedBar.latitud,updatedBar.longitud]
+    ];
+    connection.query('UPDATE ubicacion SET nombre_ubicacion = ?, latitud = ?, longitud = ? WHERE id_ubicacion = ?', [updatedBar.nombre_bar, updatedBar.latitud, updatedBar.longitud, updatedBar.id_ubicacion], (error, result) => {
+      if (error) {
+        callback(error, null);
+        return;
+      } else {
+        const bar =[[          
+          updatedBar.nombre_bar,
+          updatedBar.id_ubicacion,
+          updatedBar.desayuno_horario,
+          updatedBar.almuerzo_horario,
+          updatedBar.merienda_horario,
+        ]
+        ];  
+        connection.query('UPDATE ubicacion SET nombre_bar = ?, id_ubicacion = ?, desayuno_horario = ?, almuerzo_horario = ?, merienda_horario = ? WHERE id_bar = ?', [updatedBar.nombre_bar,updatedBar.id_ubicacion, updatedBar.desayuno_horario,updatedBar.almuerzo_horario,updatedBar.merienda_horario,id], (error, result) => {
+          if (error) {
+            callback(error, null);
+          } else {
+            callback(null, result);
+          }
+        });
+      }
+    });
   },
 
   delete: (id, callback) => {
